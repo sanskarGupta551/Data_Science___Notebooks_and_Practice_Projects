80e9563bb43b55a008f70935a201209f430ed581
sanskarGupta551 <guptasanskar551@gmail.com>
2024-03-06 14:34:17 +0530

    Streamlit Webapp

---
+++ 12_Streamlit_Webapp/Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import os
+
+
+
+class webapp():
+    def __init__(self):
+        self.sidebar()
+        
+
+
+    def sidebar(self):
+        # Sidebar navigation
+        st.sidebar.title('Navigation')
+        
+
+
+webapp()
